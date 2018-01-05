6bef1dbb279ae254c4e6ced142a0bb8428822532
YJob <472830294@qq.com>
2018-01-05 17:32:19 +0800

    2018-01-05 python笔记

--- 014 JS高级程序设计/006 函数表达式/project/project.js
+++ 014 JS高级程序设计/006 函数表达式/project/project.js
@@ -1,28 +1,5 @@
-(function () {
-  // 私有变量
-  var name = '';            //不定义，下方的name会成为全局变量
+var a = false;
+var b = 'add';
 
-  //构造函数
-  Person = function(val) {
-    name = val;
-  };
-
-  //特权方法
-  Person.prototype.getName = function() {
-    return name;
-  }
-
-  Person.prototype.setName = function(val) {
-    name = val;
-  }
-})();
-
-var person1 = new Person('WriteJob');
-console.log(person1.getName());          //WriteJob
-
-var person2 = new Person();
-console.log(person2.getName());          //undefined
-
-person2.setName('WhyJob');
-console.log(person1.getName());          //WhyJob
-console.log(person2.getName());          //WhyJob
+console.log(b && a);
+console.log(a || b);

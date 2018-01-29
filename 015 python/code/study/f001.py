d3e5c0e897bf0836cf18cf50c2024c94bc2e6419
YJob <472830294@qq.com>
2018-01-29 18:01:13 +0800

    2018-01-29 18:00

--- 015 python/code/study/f001.py
+++ 015 python/code/study/f001.py
@@ -1,11 +1,1 @@
 # -*- coding: utf-8 -*-
-d = {'Adam': 95, 'Lisa': 85, 'Bart': 59}
-tds = [
-    '<tr><td>%s</td><td>%s</td></tr>' % (name, score)
-    for name, score in d.iteritems()
-]
-print tds
-print '<table>'
-print '<tr><th>Name</th><th>Score</th><tr>'
-print '\n'.join(tds)
-print '</table>'

7ebb4c831454fbf5006605ceb6f284d38c3ced8c
Administrator <472830294@qq.com>
2018-01-30 23:04:21 +0800

    2018-01-30 23:04

--- 015 python/code/study/f001.py
+++ 015 python/code/study/f001.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+a = {1, 2, 3}
+b = {2, 1, 3}
+print(a == b)
+#  >>> True
+print(a is b)
+#  >>> False
+
+c = (1, 2, 3)
+d = (2, 1, 3)
+print(c == d)
+#  >>> False
+print(c is d)
+#  >>> False
+
+type()
